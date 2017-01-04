f330b7b729576a05e6fb07f3eef5aa6e37baf7e4
xuanbo <wxb740119>
2017-01-04 14:45:11 +0800

    dubbo-admin

--- README.md
+++ README.md
@@ -2,8 +2,22 @@
 
 dubbo 的入门例子。
 
+消费方可以使用mvc做restful服务，调用暴露的接口即可。
+
 # 流程
 
 *   首先安装 zookeeper，配置后并启动
 *   启动provider模块的App Main方法
 *   启动consumer模块的App Main方法
+
+# dubbo-admin
+
+*   github上clone dubbo源码
+*   进入dubbo-admin文件夹，mvn jetty:run -Ddubbo.registry.address=zookeeper://127.0.0.1:2181
+*   浏览器输入http://localhost:8080即可
+
+根据实际情况修改IP和端口号。
+
+# dubbo的不足
+
+任何技术都有其使用范围和局限性，dubbo也不例外。dubbo主要是对Provider提供的服务进行服务治理、负载均衡、灰度发布提供支撑；不负责consumer对外提供的服务，我们一般用nginx或者F5来实现它的负载均衡。nginx提供热reload，这样已经够用了。如果要对nginx实现负载均衡的话，可以使用DNS的方式。
--- consumer/src/main/java/xinQing/consumer/App.java
+++ consumer/src/main/java/xinQing/consumer/App.java
@@ -3,16 +3,19 @@ package xinQing.consumer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import xinQing.provider.service.ProviderService;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext(new String[] { "dubbo-consumer.xml" });
         context.start();
         ProviderService providerService = context.getBean(ProviderService.class);
         String say = providerService.say("dubbo!!");
         System.out.println(say);
+        System.in.read(); // 按任意键退出
     }
 }
